2e1ed6d34733545f61ff7cbd37d8334db0df2cc7
Netanel Davidov <Netanel.Davidov@sapiens.com>
2019-10-03 17:50:22 +0300

    update

--- PanoramaRegistrationAndStitching.py
+++ PanoramaRegistrationAndStitching.py
@@ -16,18 +16,23 @@ def findIndexsAndValuesInImg2(pos1, pos2, pos1_4, amount: int):
         ip4 = None
         for i in range(pos1.shape[0]):
             if pos1[i][0] == pos1_4[0][0] and pos1[i][1] == pos1_4[0][1]:
+                print("in1")
                 p1 = pos2[i]
                 ip1 = i
             elif pos1[i][0] == pos1_4[1][0] and pos1[i][1] == pos1_4[1][1]:
+                print("in2")
                 p2 = pos2[i]
                 ip2 = i
             elif pos1[i][0] == pos1_4[2][0] and pos1[i][1] == pos1_4[2][1]:
+                print("in3")
                 p3 = pos2[i]
                 ip3 = i
             elif pos1[i][0] == pos1_4[3][0] and pos1[i][1] == pos1_4[3][1]:
+                print("in4")
                 p4 = pos2[i]
                 ip4 = i
         real_pos2_val = np.array([p1, p2, p3, p4])
+        print("real_pos2_val = ", real_pos2_val)
         real_pos2_ind = np.array([ip1, ip2, ip3, ip4])
         return real_pos2_val, real_pos2_ind
     elif amount == 1:
@@ -40,6 +45,16 @@ def findIndexsAndValuesInImg2(pos1, pos2, pos1_4, amount: int):
         real_pos2_val = p1
         real_pos2_ind = ip1
         return real_pos2_val, real_pos2_ind
+
+
+def testTheInliers(inliers, pos1: np.ndarray, pos2: np.ndarray):
+    t1 = []
+    t2 = []
+    for i in inliers:
+        t1.append(pos1[i])
+        t2.append(pos2[i])
+    print("test : t1 = ", np.array(t1))
+    print("test : t2 = ", np.array(t2))
 
 
 # ===================================
@@ -84,8 +99,8 @@ def matchFeatures(img1: np.ndarray, img2: np.ndarray):
     #     plt.imshow(img2)
     #     plt.show()
 
-    # Display 50 matches
-    img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None, flags=2)
+    # Display 200 matches
+    img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:20], None, flags=2)
     plt.imshow(img3)
     plt.show()
 
@@ -93,64 +108,111 @@ def matchFeatures(img1: np.ndarray, img2: np.ndarray):
 
 
 def ApplyHomography(pos1: np.ndarray, H12: np.ndarray) -> np.ndarray:
-    pos2 = np.zeros(pos1.shape)
+    pos2 = None
     i = 0
-    j = 0
     for point in pos1:
         homog_point = np.array([point[0], point[1], 1])
         nhomog_point = np.dot(H12, homog_point.T)
         x = nhomog_point[0]/nhomog_point[2]
         y = nhomog_point[1]/nhomog_point[2]
         res_point = np.array([x, y])
-        pos2[i] = res_point
+        pos2 = res_point
         i += 1
     return pos2
 
 
 def leastSquareHomograpy(p1: np.ndarray, p2: np.ndarray) -> np.ndarray:
-    x1, y1 = p1[0][0], p1[0][1]
-    x2, y2 = p1[1][0], p1[1][1]
-    x3, y3 = p1[2][0], p1[2][1]
-    x4, y4 = p1[3][0], p1[3][1]
-    xp1, yp1 = p2[0][0], p2[0][1]
-    xp2, yp2 = p2[1][0], p2[1][1]
-    xp3, yp3 = p2[2][0], p2[2][1]
-    xp4, yp4 = p2[3][0], p2[3][1]
-
-    A = [
-        [-x1, -y1, -1, 0, 0, 0, x1 * xp1, y1 * xp1, xp1],
-        [0, 0, 0, -x1, -y1, -1, x1 * yp1, y1 * yp1, yp1],
-        [-x2, -y2, -1, 0, 0, 0, x2 * xp2, y2 * xp2, xp2],
-        [0, 0, 0, -x2, -y2, -1, x2 * yp2, y2 * yp2, yp2],
-        [-x3, -y3, -1, 0, 0, 0, x3 * xp3, y3 * xp3, xp3],
-        [0, 0, 0, -x3, -y3, -1, x3 * yp3, y3 * yp3, yp3],
-        [-x4, -y4, -1, 0, 0, 0, x4 * xp4, y4 * xp4, xp4],
-        [0, 0, 0, -x4, -y4, -1, x4 * yp4, y4 * yp4, yp4]]
-
-    U, S, V = np.linalg.svd(A)
-    m = min(S)
-    # print("min = ", m)
-    # print("S = ", S)
-    # print("V' = ", np.dot(V, V.T))
-
-    H = V[:, V.shape[1] - 1]
+    """
+        Finds the homography matrix, M, that transforms points from p1 to p2.
+        returns the homography and the error between the transformed points to their
+        destination (matched) points.
+        Error = np.sqrt(sum((M.dot(p1)-p2)**2))
+        p1: 4+ keypoints locations (x,y) on the original image. Shape:[4+,2]
+        p2: 4+ keypoints locations (x,y) on the destenation image. Shape:[4+,2]
+        return: (Homography matrix shape:[3,3], Homography error)
+        """
+    M = np.zeros((2 * p1.shape[0], 9))
+    i = 0
+    while i < 8:
+        M[i][0] = p1[i // 2][0]
+        M[i][1] = p1[i // 2][1]
+        M[i][2] = 1
+        M[i][6] = -1 * (p2[i // 2][0] * p1[i // 2][0])
+        M[i][7] = -1 * (p2[i // 2][0] * p1[i // 2][1])
+        M[i][8] = -1 * (p2[i // 2][0])
+        M[i + 1][3] = p1[i // 2][0]
+        M[i + 1][4] = p1[i // 2][1]
+        M[i + 1][5] = 1
+        M[i + 1][6] = -1 * (p2[i // 2][1] * p1[i // 2][0])
+        M[i + 1][7] = -1 * (p2[i // 2][1] * p1[i // 2][1])
+        M[i + 1][8] = -1 * (p2[i // 2][1])
+        i += 2
+    print("M = ", M)
+    U, D, V = np.linalg.svd(M)
+    print("D = ", D)
+    print("V = ", V)
+    H = V[:, V.shape[1] - 2]
+    print("H = ", H)
+    print("len(H) = ", len(H))
+    H = H / H[len(H) - 1]
+    print("H = ", H)
     H = np.reshape(H, (3, 3))
-    # print("H = ", H)
 
-    # Test the result matrix
-    t_H, mask = cv2.findHomography(p1, p2, method=0)
-    print("t_H (homography matrix)= ", t_H)
-    return t_H
+    H12, mask = cv2.findHomography(p1, p2)
+    print("opencv = ", H12)
+
+    print("[p1[0][0], p1[0][1], 1] = ", [p1[0][0], p1[0][1], 1])
+    p_1 = np.dot(H, [p1[0][0], p1[0][1], 1])
+    p_11 = np.dot(H12, [p1[0][0], p1[0][1], 1])
+
+    print(p_1/p_1[2], p_11/p_11[2])
+
+    # x1, y1 = p1[0][0], p1[0][1]
+    # x2, y2 = p1[1][0], p1[1][1]
+    # x3, y3 = p1[2][0], p1[2][1]
+    # x4, y4 = p1[3][0], p1[3][1]
+    # xp1, yp1 = p2[0][0], p2[0][1]
+    # xp2, yp2 = p2[1][0], p2[1][1]
+    # xp3, yp3 = p2[2][0], p2[2][1]
+    # xp4, yp4 = p2[3][0], p2[3][1]
+    #
+    # A = [
+    #     [-x1, -y1, -1, 0, 0, 0, x1 * xp1, y1 * xp1, xp1],
+    #     [0, 0, 0, -x1, -y1, -1, x1 * yp1, y1 * yp1, yp1],
+    #     [-x2, -y2, -1, 0, 0, 0, x2 * xp2, y2 * xp2, xp2],
+    #     [0, 0, 0, -x2, -y2, -1, x2 * yp2, y2 * yp2, yp2],
+    #     [-x3, -y3, -1, 0, 0, 0, x3 * xp3, y3 * xp3, xp3],
+    #     [0, 0, 0, -x3, -y3, -1, x3 * yp3, y3 * yp3, yp3],
+    #     [-x4, -y4, -1, 0, 0, 0, x4 * xp4, y4 * xp4, xp4],
+    #     [0, 0, 0, -x4, -y4, -1, x4 * yp4, y4 * yp4, yp4]]
+    #
+    # U, S, V = np.linalg.svd(A)
+    # m = min(S)
+    # # print("min = ", m)
+    # # print("S = ", S)
+    # # print("V' = ", np.dot(V, V.T))
+    #
+    # H = V[:, V.shape[1] - 1]
+    # H = np.reshape(H, (3, 3))
+    # # print("H = ", H)
+
+    # # Test the result matrix
+    # try:
+    #     t_H, mask = cv2.findHomography(p1, p2, method=0)
+    #     print("t_H (homography matrix)= ", t_H)
+    #     return t_H
+    # except Exception as e:
+    #     print("exception = ", p1, p2)
+    return H
 
 
 def E(H12: np.ndarray, pos1_1: np.ndarray, pos1: np.ndarray, pos2: np.ndarray, inlierTol: int) -> (int, int):
     """
-    The method find the inliers and outliers of matching points for given set of
-    4 points.
+    The method find the inliers and outliers of matching points for given of 1 point.
     The method use Squared Euclidean Distance to find inliers points :
     E_i = || P' - P ||^2 < inlierTol
-    when P' - is the transformed set of 4 points
-    and P - is the 4 matching point in image 2
+    when P' - is the transformed set of 1 point
+    and P - is the matching point in image 2
     :param H12: Homography matrix
     :param pos1_1: Set of 1 point in image 1
     :param pos1: Full set of points in image 1
@@ -173,20 +235,19 @@ def E(H12: np.ndarray, pos1_1: np.ndarray, pos1: np.ndarray, pos2: np.ndarray, i
     if sed < inlierTol:
         return 1, real_pos2_ind
     else:
-        return 0
+        return 0, None
 
 
 def ransacHomography(pos1: np.ndarray, pos2: np.ndarray, numIter: int, inlierTol: int) -> (np.ndarray, np.ndarray):
-    maxIliners = []
+    maxInliers = []
     for i in range(numIter):
         r_p1 = random.choice(pos1)
-        r_p2 = random.choice(pos1)
-        r_p3 = random.choice(pos1)
-        r_p4 = random.choice(pos1)
-        print("r_p1 = ", r_p1)
-        print("r_p2 = ", r_p2)
-        print("r_p3 = ", r_p3)
-        print("r_p4 = ", r_p4)
+        r_p2 = random.choice([a for a in pos1 if a[0] != r_p1[0] and a[1] != r_p1[1]])
+        r_p3 = random.choice([a for a in pos1 if a[0] != r_p1[0] and a[1] != r_p1[1]
+                              and a[0] != r_p2[0] and a[1] != r_p2[1]])
+        r_p4 = random.choice([a for a in pos1 if a[0] != r_p1[0] and a[1] != r_p1[1]
+                              and a[0] != r_p2[0] and a[1] != r_p2[1]
+                              and a[0] != r_p3[0] and a[1] != r_p3[1]])
         rp_4 = np.array([r_p1, r_p2, r_p3, r_p4])
         real_pos2_val, real_pos2_ind = findIndexsAndValuesInImg2(pos1, pos2, rp_4, 4)
         H = leastSquareHomograpy(rp_4, real_pos2_val)
@@ -199,16 +260,22 @@ def ransacHomography(pos1: np.ndarray, pos2: np.ndarray, numIter: int, inlierTol
                 inliers.append(match)
 
         # Find the maximum inliers set
-        print("len(inliers) = ", len(inliers))
-        if len(inliers) > len(maxIliners):
-            maxIliners = inliers
-            print("maxIliners = ", maxIliners)
+        if len(inliers) > len(maxInliers):
+            maxInliers = inliers
 
     # Calculate the finally homography matrix by inliers
-    # /////////////////your code//////////////////
+    print("maxInliers = ", maxInliers)
+    t_pos1 = np.zeros((len(maxInliers), 2))
+    t_pos2 = np.zeros((len(maxInliers), 2))
+    ind = 0
+    for i in maxInliers:
+        t_pos1[ind][0], t_pos1[ind][1] = (pos1[i][0], pos1[i][1])
+        t_pos2[ind][0], t_pos2[ind][1] = (pos2[i][0], pos2[i][1])
+        ind += 1
+    H12 = leastSquareHomograpy(t_pos1, t_pos2)
 
     # return the finally homography matrix and the set of maxInliers
-    return maxIliners
+    return H12, maxInliers
 
 
 # ===================================
@@ -219,10 +286,10 @@ img2 = cv2.imread('backyard2.jpg')
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 pos1, pos2 = matchFeatures(img1, img2)
-# t_p1 = np.array([pos1[0], pos1[1], pos1[2], pos1[3]])
-# t_p2 = np.array([pos2[0], pos2[1], pos2[2], pos2[3]])
-# H = leastSquareHomograpy(t_p1, t_p2)
+t_p1 = np.array([pos1[0], pos1[1], pos1[2], pos1[3]])
+t_p2 = np.array([pos2[0], pos2[1], pos2[2], pos2[3]])
+H = leastSquareHomograpy(t_p1, t_p2)
 # count = E(H, t_p1, pos1, pos2, 1)
 # print(count)
-ransacHomography(pos1, pos2, 20, 1)
-# print(t_pos2)
+# h, inliers = ransacHomography(pos1, pos2, 10, 1)
+# testTheInliers(inliers, pos1, pos2)
